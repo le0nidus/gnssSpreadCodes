a302b792e35b9d49c54d3a13f4c734de95fdda23
Le0nidus <liran3t@gmail.com>
2024-07-02 22:49:21 +0300

    Big change to main.cpp! implemented all the print changes!

--- main.cpp
+++ main.cpp
@@ -9,46 +9,50 @@
 
 
 int main() {
-    /*
-    std::ofstream myFile("secondaryCodePilot.txt");
-    std::vector<int> secondaryCodePilot;
-    std::vector<int> primaryCodePilot;
-    std::vector<int> primaryCodeData;
-    int j;
-	*/
-    /*
-    B1C B1C_spreadCode;
-    for (int prn = 1; prn < BEIDOU_B1C_NBR_SATS + 1; prn++) {
-        secondaryCodePilot = B1C_spreadCode.generateSecondaryPilot(prn);
-        for (j = 0; j < BEIDOU_B1C_SECONDARY_CODE_LENGTH; j++) {
-            myFile << secondaryCodePilot[j];
-        }
-        myFile << std::endl;
-    }
-    */
-	std::ofstream myfile;
-	myfile.open("GPS_L5IQ.txt");
-	int i;
 	std::vector<int> vec;
-	std::vector<int> result(13);
+	int prn;
+	B1C B1C_spreadCode;
 	GPS_L5 gpsl5;
-
+	std::cout << "/===========BeiDou B1C Code===========/\n";
+	std::cout << "Secondary Pilot Code\n";
+	for (prn = 1; prn < BEIDOU_B1C_NBR_SATS + 1; prn++) {
+		std::cout << "PRN: " << prn << std::endl;
+		vec = B1C_spreadCode.generateSecondaryPilot(prn);
+		B1C_spreadCode.print(vec);
+		std::cout << std::endl;
+	}
+	std::cout << "Primary Pilot Code\n";
+	for (prn = 1; prn < BEIDOU_B1C_NBR_SATS + 1; prn++) {
+		std::cout << "PRN: " << prn << std::endl;
+		vec = B1C_spreadCode.generatePrimaryPilot(prn);
+		B1C_spreadCode.print(vec);
+		std::cout << std::endl;
+	}
+	std::cout << "Primary Data Code\n";
+	for (prn = 1; prn < BEIDOU_B1C_NBR_SATS + 1; prn++) {
+		std::cout << "PRN: " << prn << std::endl;
+		vec = B1C_spreadCode.generatePrimaryData(prn);
+		B1C_spreadCode.print(vec);
+		std::cout << std::endl;
+	}
+    
+	std::cout << std::endl;
+	std::cout << "/===========GPS L5 Code===========/\n";
 	std::cout << "L5Q CODE\n";
-	for (i = 1; i < 64; i++) {
-		vec = gpsl5.generateL5Q(i);
-		std::cout << "PRN: " << i << std::endl;
+	for (prn = 1; prn < 64; prn++) {
+		vec = gpsl5.generateL5Q(prn);
+		std::cout << "PRN: " << prn << std::endl;
 		gpsl5.print(vec, ",");
 		std::cout << std::endl;
 	}
 
 	std::cout << "L5I CODE\n";
-	for (i = 1; i < 64; i++) {
-		vec = gpsl5.generateL5I(i);
-		std::cout << "PRN: " << i << std::endl;
+	for (prn = 1; prn < 64; prn++) {
+		vec = gpsl5.generateL5I(prn);
+		std::cout << "PRN: " << prn << std::endl;
 		gpsl5.print(vec);
 		std::cout << std::endl;
 	}
-	myfile.close();
 
     return 0;
 }
